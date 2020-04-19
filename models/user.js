7625ee7fbd5205b48c9ef2f4c7c587b800914af0
Martin Daniel <martindaniel555@gmail.com>
2020-04-19 17:31:36 +0300

    Create user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  password: String,
+});
+
+const model = mongoose.model('user', userSchema);
+
+module.exports = model;
