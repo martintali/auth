e1bdd147f7da40ddb767aaca3efc2fad1dc04249
Martin Daniel <martindaniel555@gmail.com>
2020-04-19 17:30:05 +0300

    Init auth server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParseer = require('body-parser');
+const router = require('./router');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParseer.json({ type: '*/*' }));
+router(app);
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server listening on port ${port}`);
