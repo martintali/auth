25dded99f525010ca690479390e4833bc4271ced
Martin Daniel <martindaniel555@gmail.com>
2020-04-20 11:54:03 +0300

    Return token when signup

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,17 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode(
+    {
+      sub: user.id,
+      iat: timestamp,
+    },
+    config.secret
+  );
+}
 
 exports.signup = function (req, res, next) {
   const { email, password } = req.body;
@@ -17,7 +30,7 @@ exports.signup = function (req, res, next) {
     newUser.save((err) => {
       if (err) return next(err);
 
-      res.json({ success: true });
+      res.json({ token: tokenForUser(newUser) });
     });
   });
 };
